2e6b3d9d665f760d723496b06658f8947857d9a1
rinron <github4273senyou564@gmail.com>
2025-11-27 00:34:18 +0900

    Add

--- CSVsys.py
+++ CSVsys.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-# SPDX-FileCopyrightText: Rintarou Matunaga
+# SPDX-FileCopyrightText: 2025 Rintarou Matunaga
 # SPDX-License-Identifier: BSD-3-Clause
 
 import sys
 import csv
-from CSVfiltersys import filter_rows, sort_rows
 
 def main():
     reader = csv.reader(sys.stdin)
-    rows = list(reader)
-
-    header = rows[0]
-    body = rows[1:]
-
-    filtered = filter_rows(body)
-    sorted_rows = sort_rows(filtered)
-
     writer = csv.writer(sys.stdout)
-    writer.writerow(header)
-    writer.writerows(sorted_rows)
+
+    for row in reader:
+        writer.writerow(row)
 
 if __name__ == "__main__":
     main()
