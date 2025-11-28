0162e46541e5a3db14b39e3f7ad4928483fdca6b
rinron <github4273senyou564@gmail.com>
2025-11-28 15:30:38 +0900

    Add

--- test.bash
+++ test.bash
@@ -3,14 +3,28 @@
 #SPDX-License-Identifier: GPL-3.0-only
 
 ng () {
-	echo ${1}行目に不備あり
-	res=1
+    echo ${1}行目に不備あり
+    res=1
 }
 
 res=0
 
-out=$(seq 5 | ./csv_sum_avg.py)
+# plus
+out=$(seq 5 | ./calc plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+# minus
+out=$(seq 5 | ./calc minus)
+[ "${out}" = -13 ] || ng "$LINENO"
+
+# times
+out=$(seq 5 | ./calc times)
+[ "${out}" = 120 ] || ng "$LINENO"
+
+# div
+out=$(seq 5 | ./calc div)
+[ "${out}" = 0.008333333333333333 ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
+
